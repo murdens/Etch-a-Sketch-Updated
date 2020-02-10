324a577b5725aa35cd10fe2d05d7a7d497c71465
murdens <sonya.murden@gmail.com>
2020-02-10 13:31:13 +0000

    @TheOdinProject etch-a-sketch updated first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+@TheOdinProject 
+
+Etch-a-Sketch Updated
+
+Assignment:
+
+- Create a grid using JS and append to HTML container
+- Change colour of the divs in grid as mouse hovers, include random RGB values also.
+- Add prompt to ask user how many squares per grid
+- run functions from click of button tags in HTML
+
+
+Additions to first repo
+- Refactored button press event code.
+- Added canvas size button so now allows user to combine default grey and colour on canvas
+- Attempted to allow touch screen event - not as responsive as needed though.  Potentially more options if use frameworks?
---
+++ script.js
@@ -0,0 +1,91 @@
+//variables
+const canvasButton = document.querySelector('#canvas');
+const clearButton = document.querySelector('#clear');
+const fillAction = document.querySelectorAll('.fill');
+const container = document.querySelector('#container');
+
+// used to ensure grid in place as window loads
+function createGrid(gridSize) {
+  container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+    
+    for (let i = 0; i < gridSize **2; i++) {
+      const grid = document.createElement('div');
+      container.appendChild(grid);
+      grid.classList.add('box');
+    }
+}
+createGrid(16);
+
+
+clearButton.addEventListener('click', (e) => {
+  clear()
+})
+
+function clear(){
+  for (let i = 0; i < matrix.length; i++) {
+    matrix[i].style.backgroundColor = 'lightgrey';
+  }
+}
+
+canvasButton.addEventListener('click', (e) => {
+  let gridSize = prompt('selet a grid size between 1 and 100');
+  inputCheck(gridSize);
+  createGrid(gridSize);
+})
+
+fillAction.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    fill = button.id;
+     console.log(fill); 
+    if (fill == 'draw'){
+        fillMatrix();    
+    } else if (fill == 'colour'){
+        fillcolorMatrix();
+    }
+  })
+})                          
+                    
+// decided to include error handling for user experience
+function inputCheck(gridSize){
+  if (gridSize < 1 || gridSize > 100 || isNaN(gridSize)){
+    alert ('Needs to be a number between 1 and 100');
+  } else { 
+    return gridSize;
+  }
+}
+
+const matrix = document.getElementsByClassName('box');
+
+function fillMatrix(){
+  for (let i = 0; i < matrix.length; i++) { 
+    matrix[i].addEventListener(touchEvent(), function() {
+    matrix[i].classList.add('active');
+    matrix[i].style.backgroundColor = 'darkslategrey';
+    });
+  }
+}
+
+function fillcolorMatrix(){
+  for (let i = 0; i < matrix.length; i++) {
+    matrix[i].addEventListener(touchEvent(), function() {
+    matrix[i].classList.add('active');
+    matrix[i].style.backgroundColor= random_bg_color();
+    });
+  }
+}
+
+function random_bg_color() {
+  var x = Math.floor(Math.random() * 256);
+  var y = Math.floor(Math.random() * 256);
+  var z = Math.floor(Math.random() * 256);
+         
+  return "rgb(" + x + "," + y + "," + z + ")";
+}
+
+function touchEvent(){
+  if ('ontouchstart' in document.documentElement === true)
+    return 'touchmove';
+  else 
+    return 'mouseenter';
+}
